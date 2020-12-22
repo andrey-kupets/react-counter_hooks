c5d47cd2053d20285caaa708be80736af24d8338
freefireman <beluh.c.s@gmail.com>
2020-12-22 21:19:29 +0200

    little fix))

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, {useState, useEffect, useReducer, useCallback, memo, useMemo} from 'react';
+import React from 'react';
 import Counter from "./components/counter/Counter";
 // import './App.css';
 
 export default function App() {
-
-
-
 
   return (
       <div className="App">
