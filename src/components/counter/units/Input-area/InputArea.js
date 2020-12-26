2bcdce89bb54b865003d86579a9189d5386c0a74
andrey-kupets <andrey.kupets@i.ua>
2020-12-26 19:22:20 +0300

    add label

--- src/components/counter/units/Input-area/InputArea.js
+++ src/components/counter/units/Input-area/InputArea.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function CreateInputArea({submitFn, valueInput, setValueInput}) {
     return (
         <div>
+            <label>Input Number</label>
             <input type={'number'} onChange={({target: {value}}) => {
                 setValueInput(+value)
             }} className='input' value={valueInput}/>
